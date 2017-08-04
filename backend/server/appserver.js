ada535941a6718dec13ac0bab2eae6b6ee3bfad5
Christian <cbenseler@gmail.com>
2017-08-04 15:49:56 -0300

    fix: CORS enable

--- backend/server/appserver.js
+++ backend/server/appserver.js
@@ -3,10 +3,20 @@ const bodyParser = require('body-parser')
 
 let drugstores = []
 
+const allowCrossDomain = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+
 const Appserver = function({ port, drugstoreService }) {
   const app = express()
 
-  app.use( bodyParser.json() )
+  app.use(bodyParser.json())
+  app.use(allowCrossDomain)
 
   app.listen(port)
 
