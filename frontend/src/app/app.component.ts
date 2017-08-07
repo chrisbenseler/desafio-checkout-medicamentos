0325cc2d48cb2afe45cbbc756cf567dd0d1e835a
Christian <cbenseler@gmail.com>
2017-08-07 08:57:12 -0300

    feat: back button & montserrat font

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +10,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Dr. Memed';
 
-  constructor(
-  ) {
+  hasBackButton = false;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location
+  ) {
+    //console.log(this.route.pathFromRoot)
+    //this.route.
+    this.route.url.subscribe( url => {
+      //console.log(url)
+    })
+
+    this.router.navigated
+
+    this.router.events.subscribe( (e: any) => {
+      if(this.router.navigated) {
+        this.router.navigated = false;
+        this.hasBackButton = e.url.indexOf('prescription') < 0;
+      }
+    })
+  }
+
+  back(event) {
+    this.location.back()
   }
 }
